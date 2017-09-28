4416c9740fcab6ccb32a3dd641597eba13649145
Shreyas jejurkar <shreyasjejurkar123@live.com>
2017-09-28 23:45:53 +0530

    Added code logic for EditPersonalDetails

--- WPFBankApplication/EditPersonalDetails.xaml.cs
+++ WPFBankApplication/EditPersonalDetails.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using ExtraTools;
 using java.lang;
 using java.sql;
@@ -23,7 +15,7 @@ namespace WPFBankApplication
     /// <summary>
     /// Interaction logic for EditPersonalDetails.xaml
     /// </summary>
-    public partial class EditPersonalDetails : Window
+    public partial class EditPersonalDetails
     {
         private OpenFileDialog _fileDialog;
         public string accc;
@@ -33,6 +25,7 @@ namespace WPFBankApplication
         {
             InitializeComponent();
             accc = accountNumber;
+            GetDetails();
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -72,44 +65,66 @@ namespace WPFBankApplication
             if (DoDataValidation())
             {
                 SaveDataToDatabase();
-               
             }
             
         }
 
         private void SaveDataToDatabase()
         {
-            string fullName = textBox_firstname.Text + " " + textBox_lastname.Text;
-            /*
+            string fullName = TextBoxName.Text;
+            
             try
             {
                 Class.forName("com.mysql.jdbc.Driver");
                 Connection c = (Connection)DriverManager.getConnection("jdbc:mysql://localhost/bankapplication", "root", "9970209265");
 
-                java.sql.PreparedStatement ps = c.prepareStatement("insert into info(Name,Address,phone_number,Email,Password,account_number,Balance,ImagePath,Gender,MobileVerification,BirthDate)values(?,?,?,?,?,?,'100',?,?,?,?)");
+                java.sql.PreparedStatement ps = c.prepareStatement("update info set Name = ? ,  Address = ? , phone_number = ? , Email = ? , ImagePath = ? , BirthDate = ? where account_number = ? ");
                 ps.setString(1, fullName);
                 ps.setString(2, textBox_address.Text);
                 ps.setString(3, textBox_phonenumber.Text);
                 ps.setString(4, textBox_email.Text);
-                ps.setString(5, textBox_pass.Password);
-                ps.setString(6, accc.ToString());
-                ps.setString(7, imageFilePath);
-                ps.setString(8, GetGenderInfo());
-                ps.setString(9, EnableMobileNotifications());
-                ps.setString(10, myDatePicker.Text);
+                ps.setString(5, imageFilePath);
+                ps.setString(6, myDatePicker.Text);
+                ps.setString(7, accc);
                 ps.executeUpdate();
                 c.close();
-                DialogBox.Show("Sucess", "Account created sucessfully", "OK");
-
-                DialogBox.Show("Sucess", "Your account number is " + accc, "OK");
+                DialogBox.Show("Sucess", "Changes done sucessfully", "OK");
             }
             catch (SQLException exception)
             {
                 DialogBox.Show("Error", "Something went wrong." + exception.Message, "OK");
             }
-            */
         }
 
+        public void GetDetails()
+        {
+            try
+            {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection c = (Connection)DriverManager.getConnection("jdbc:mysql://localhost/bankapplication", "root", "9970209265");
+
+                java.sql.PreparedStatement ps = c.prepareStatement("select * from info where account_number = ?");
+                ps.setString(1,accc);
+                ResultSet rs = ps.executeQuery();
+                ImageSourceConverter img = new ImageSourceConverter();
+
+                while (rs.next())
+                {
+                    TextBoxName.Text = rs.getString("Name");
+                    textBox_address.Text = rs.getString("Address");
+                    textBox_email.Text = rs.getString("Email");
+                    textBox_phonenumber.Text = rs.getString("phone_number");
+                    myDatePicker.Text = rs.getString("BirthDate");
+                    AccountHolderImage.SetValue(Image.SourceProperty, img.ConvertFromString(rs.getString("ImagePath")));
+                }
+            }
+            catch (SQLException exception)
+            {
+                DialogBox.Show("Error", "Something went wrong." + exception.Message, "OK");
+            }
+        }
+
+        
         private bool DoDataValidation()
         {
             //saving phone number leangh into a length variable
@@ -118,10 +133,9 @@ namespace WPFBankApplication
             //checking email validation which returns bool value 
             bool isEmailValid = textBox_email.Text.Contains("@");
             bool isEmailValid2 = textBox_email.Text.Contains(".com");
-
-
+            
             // Is there any textbox is empty or not. If there then it will fire error message
-            if (textBox_firstname.Text == "" || textBox_lastname.Text == "" || textBox_email.Text == "" || textBox_address.Text == "" || textBox_phonenumber.Text == "" || AccountHolderImage.Source == null || myDatePicker.Text == "")
+            if (TextBoxName.Text == "" || textBox_email.Text == "" || textBox_address.Text == "" || textBox_phonenumber.Text == "" || AccountHolderImage.Source == null || myDatePicker.Text == "")
             {
                 DialogBox.Show("Error", "Please enter all field", "OK");
                 return false;
