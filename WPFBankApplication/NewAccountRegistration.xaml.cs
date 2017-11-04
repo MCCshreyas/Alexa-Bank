0e633297d6f542510d43e276a19747a3f6fcf605
Shreyas jejurkar <shreyasjejurkar123@live.com>
2017-11-04 23:40:27 +0530

    errors

--- WPFBankApplication/NewAccountRegistration.xaml.cs
+++ WPFBankApplication/NewAccountRegistration.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
 using ExtraTools;
 using java.lang;
 using java.sql;
